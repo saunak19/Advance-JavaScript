e81721f1715b11fa26a9152ff2eb1c1057520ea3
saunak19 <saunak.nxs@gmail.com>
2025-06-13 14:19:27 +0530

    swipe array

--- Objects/App.js
+++ Objects/App.js
@@ -1,1 +1,13 @@
-console.log('MY FIRST APP')
+const arr = [10, 5, 4, 8, 3, 6, 2, 7, 1, 9];
+//sort array
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 1; j < i; j++) {
+    if (arr[j] < arr[i]) {
+      //   [arr[i], arr[j]] =[arr[j],arr[i]]
+      let swipe = arr[i];
+      arr[i] = arr[j];
+      arr[j] = swipe;
+    }
+  }
+}
+console.log(arr);
